02b9de54f19057cee3552d0865409c8aeb816d77
andrea rota <a@xelera.eu>
2011-09-13 11:00:54 +0100

    initial authors-ui code

---
+++ lc-pods-ui/authors-ui.php
@@ -0,0 +1,44 @@
+<?php
+/*
+Plugin Name: Authors Pods UI
+Plugin URI: http://lsecities.net/labs/
+Description: Customized Pods UI for Authors
+Version: 0.1
+Author: Andrea Rota
+Author URI: http://lsecities.net/
+*/
+
+function pods_ui_authors()
+{
+  $icon = '';
+  add_object_page('Author', 'Author', 'read', 'author', '', $icon);
+  add_submenu_page('author', 'Author', 'Author', 'read', 'author', 'author_page');
+}
+
+function author_page()
+{
+  $object = new Pod('authors');
+  $add_fields = $edit_fields = array(
+                    'name',
+                    'family_name',
+                    'profile_text',
+                    'organization',
+                    'role');
+  $object->ui = array(
+                    'title'   => 'Author',
+                    'columns' => array(
+                              'name'         => 'Name',
+                              'family_name'  => 'Family name',
+                              'organization' => 'Organization',
+                              'created'      => 'Date Created',
+                              'modified'     => 'Last Modified'
+                              ),
+                    'add_fields'  => $add_fields,
+                    'edit_fields' => $edit_fields
+					);
+  pods_ui_manage($object);
+}
+
+add_action('admin_menu','pods_ui_authors');
+
+?>
