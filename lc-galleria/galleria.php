7a3230fd66d8b8e7c4954b0a08f4ea20d16983f7
andrea rota <a@xelera.eu>
2011-10-18 16:52:35 +0100

    added support for picasa plugin

--- lc-galleria/galleria.php
+++ lc-galleria/galleria.php
@@ -8,7 +8,7 @@ Author: Michael Tyson
 Author URI: http://atastypixel.com/blog
 */
 
-function galleria_shortcode($args) {
+function galleria_orig_shortcode($args) {
     ob_start();
     ?>
     <div id="galleria"></div>
@@ -40,7 +40,7 @@ function galleria_shortcode($args) {
     return $c;
 }
 
-function galleria_photosets_shortcode($args) {
+function galleria__orig_photosets_shortcode($args) {
     ob_start();
     ?>
     <div id="galleria_photosets"></div>
@@ -55,17 +55,24 @@ function galleria_photosets_shortcode($args) {
     return $c;
 }
 
-function galleria_test_shortcode($args) {
+function galleria_shortcode($args) {
   ob_start();
   ?>
   <div id="galleria" style="height: <?php echo ($args['height'] ? $args['height'] : '300'); ?>px;"></div>
   <script type="text/javascript">
   jQuery(document).ready(function() {
-    jQuery('#galleria').galleria({
+    jQuery('#galleria').galleria({ 
+      <?php if($args['picasa_album']) : ?>
+      picasa: 'useralbum:<?php echo $args['picasa_album'] ; ?>',
+          picasaOptions: {
+        sort: 'date-posted-asc'
+      }
+      <?php elseif($args['flickr_set_id']) : ?>
       flickr: 'set:<?php echo $args['flickr_set_id'] ?>',
       flickrOptions: {
         sort: 'date-posted-asc'
       }
+      <?php endif ;
     });
   });
   </script>  
@@ -79,13 +86,14 @@ function galleria_init() {
   wp_enqueue_script('jquery');
 	wp_enqueue_script('galleria', WP_PLUGIN_URL.'/lc-galleria/galleria/galleria-1.2.5lc0.min.js', 'jquery');
 	wp_enqueue_script('galleria_flickr', WP_PLUGIN_URL.'/lc-galleria/galleria/plugins/flickr/galleria.flickr.min.js', 'galleria');
+  wp_enqueue_script('galleria_picasa', WP_PLUGIN_URL.'/lc-galleria/galleria/plugins/picasa/galleria.picasa.min.js', 'galleria');
   wp_enqueue_script('galleria_theme_classic', WP_PLUGIN_URL.'/lc-galleria/galleria/themes/classic/galleria.classic.min.js', 'galleria');
 	wp_enqueue_script('galleria_extras', WP_PLUGIN_URL.'/lc-galleria/galleria_extras.js', 'jquery');
 }
 
 add_action('init', 'galleria_init' );
+add_shortcode('galleria_orig', 'galleria_orig_shortcode');
+add_shortcode('galleria_orig_photosets', 'galleria_orig_photosets_shortcode');
 add_shortcode('galleria', 'galleria_shortcode');
-add_shortcode('galleria_photosets', 'galleria_photosets_shortcode');
-add_shortcode('galleria_test', 'galleria_test_shortcode');
 
 ?>
