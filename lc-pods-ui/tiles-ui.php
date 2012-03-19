f01c108f3dfc5dcb7d0e14d9cc1ef688fb783f97
andrea rota <a.rota@lse.ac.uk>
2012-03-19 14:52:54 +0000

    new plugin

---
+++ lc-pods-ui/tiles-ui.php
@@ -0,0 +1,56 @@
+<?php
+/*
+Plugin Name: LSE Cities Tiles Pods UI
+Plugin URI: http://lsecities.net/labs/
+Description: Customized Pods UI for Tiles
+Version: 0.2
+Author: Andrea Rota
+Author URI: http://lsecities.net/
+*/
+
+function pods_ui_tiles()
+{
+  $icon = '';
+  add_object_page('Tiles', 'Tiles', 'read', 'tiles', '', $icon);
+  add_submenu_page('tiles', 'Tiles', 'Tiles', 'read', 'tiles', 'tile_page');
+}
+
+function tile_page()
+{
+  $object = new Pod('tiles');
+  $add_fields = $edit_fields = array(
+                    'slug',
+                    'name',
+                    'tile_layout',
+                    'class',
+                    'target_page',
+                    'target_uri',
+                    'image',
+                    'tagline',
+                    'blurb'
+                    'family');
+  $object->ui = array(
+                    'title'   => 'Tile',
+                    'reorder' => 'displayorder',
+                    'reorder_columns' => array(
+                      'name' => 'Title',
+                      'tagline'      => 'Subtitle',
+                      'tile_layout'  => 'Layout'
+                    ),
+                    'columns' => array(
+                              'name'         => 'Title',
+                              'tagline'      => 'Subtitle',
+                              'tile_layout'  => 'Layout',
+                              'class'        => 'Extra classes',
+                              'created'      => 'Date Created',
+                              'modified'     => 'Last Modified'
+                              ),
+                    'add_fields'  => $add_fields,
+                    'edit_fields' => $edit_fields
+					);
+  pods_ui_manage($object);
+}
+
+add_action('admin_menu','pods_ui_tiles');
+
+?>
